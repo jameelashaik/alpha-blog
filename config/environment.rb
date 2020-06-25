1099ecd3fc31b3546a4af2951ce0fdb4d3a061e1
jameela shaik <jameelashaik7799@gmail.com>
2020-06-25 12:24:26 +0530

    clean code

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
+ActionView::Base.field_error_proc = proc.new do |html_tag, instance|
+  html_tag.html_safe 
+end  
